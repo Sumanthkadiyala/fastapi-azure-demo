b3f661a3bf054acc413bd04cbfca988480737519
SumanthKadiyala <sumanthkadiyala7@gmail.com>
2026-04-23 10:46:45 +0530

    updated fastapi app

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+# create app
 app = FastAPI()
 
+# Hello World endpoint
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
+
+# Health check endpoint
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
-
-@app.get("/hello")
-def hello():
-    return {"message": "Hello World"}
